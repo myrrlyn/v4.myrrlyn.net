048327b653d6be981f1f061e1c18565f609542f4
myrrlyn <self@myrrlyn.net>
2024-09-01 01:33:50 -0500

    Create a support package

---
+++ apps/wyz/lib/wyz.ex
@@ -0,0 +1,18 @@
+defmodule Wyz do
+  @moduledoc """
+  Support utilities for the website
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wyz.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/wyz/mix.exs
@@ -0,0 +1,43 @@
+defmodule Wyz.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :wyz,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+
+      # Documentation
+      name: "Wyz",
+      source_url: "https://github.com/myrrlyn/v4.myrrlyn.net",
+      homepage_url: "https://myrrlyn.net/elixir/wyz",
+      docs: [
+        main: "Wyz",
+        extras: ["README.md"]
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+    ]
+  end
+end
